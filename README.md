b5f3f17d8445e7363a23bb354793aa9003244e88
reshu101 <reshmasingh018@gmail.com>
2023-12-28 10:34:26 +0530

    add new file

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Shopping-Cart
+Key Points:
+
+Product Display: The layout includes a clear representation of the product with its main image and thumbnail images for easy navigation.
+
+Product Information: Details like the product vendor, title, price, and a compare-at price (with an example of $199) are displayed. Additionally, the percentage off is calculated using JavaScript.
+
+Variant Selectors: Color and size selectors are provided, allowing users to choose their preferred options.
+
+Quantity Selector: A quantity selector is available for users to specify the desired quantity of the product.
+
+Add to Cart Button: A button to add the selected product to the cart is present.
+
+Cart Message: Upon clicking the "Add to Cart" button, a message is displayed, showcasing the added product's details, as shown in the design.
+
+Responsiveness: The webpage is designed to be responsive across various devices, including mobile screens (up to 767px), tablet screens (768px to 1024px), and desktop screens (above 1025px).
+
+The implementation utilizes HTML for structuring the content, CSS for styling and layout, and JavaScript for dynamic functionalities like calculating the percentage off and handling user interactions.
---
+++ script.js
@@ -0,0 +1,63 @@
+// Sample product data. Replace this with your actual API call.
+const productData = {
+  vendor: "Marmeto",
+  title: "Embrace Sideboard",
+  price: 12999,
+  compareAtPrice: 19999,
+  colors: ["yellow", "green", "blue", "pink"],
+  sizes: ["Small", "Medium", "Large", "extra-large", "XXL"],
+  description:
+    "The Embrace Sideboard is a stylish wear. With a top cloth designed to provide superior protection and look great, this storage solution is both functional and attractive. It fits seamlessly into any home decor, with clean lines and a timeless look. Crafted from premium materials for a combination of style, durability, and reliability.",
+};
+
+document.getElementById("product-vendor").innerText = ` ${productData.vendor}`;
+document.getElementById("product-title").innerText = productData.title;
+
+document.getElementById(
+  "current-price"
+).innerText = `disconted price $${productData.price}`;
+document.getElementById(
+  "compare-at-price"
+).innerText = `price $${productData.compareAtPrice}`;
+
+document.getElementById("description").innerText = productData.description;
+
+function addToCart() {
+  const productName = document.getElementById("product-title").innerText;
+  const productColor = document.getElementById("color").value;
+  const selectedSize = document.querySelector(
+    'input[name="size"]:checked'
+  ).value;
+
+  const cartMessage = document.getElementById("cart-message");
+  cartMessage.innerText = `${productName} with color ${productColor}, Size: ${selectedSize} added to the cart`;
+  cartMessage.style.display = "block";
+
+  setTimeout(() => {
+    cartMessage.style.display = "none";
+  }, 3000);
+}
+function updateSelectedColor() {
+  const colorSelector = document.getElementById("color");
+  const selectedColor = colorSelector.value;
+  const colorContainer = document.getElementById("selected-color-container");
+
+  colorContainer.style.backgroundColor = selectedColor;
+}
+function updateSelectedSize(selectedSize) {}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const originalPriceElement = document.querySelector(".current-price");
+  const discountedPriceElement = document.getElementById("compare-at-price");
+
+  const originalPrice = 19999; // Replace with your actual original price
+  const discountPercentage = 35; // Replace with your actual discount percentage
+
+  // Calculate discounted price
+  const discountedPrice =
+    originalPrice - (originalPrice * discountPercentage) / 100;
+
+  // Update HTML content
+  originalPriceElement.innerText = `  $${originalPrice}`;
+  discountedPriceElement.innerText = `$${discountedPrice.toFixed(2)}`;
+});
